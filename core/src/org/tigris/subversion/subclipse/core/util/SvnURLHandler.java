c7026421dfdaca89ade6817d6033d6103348071a
Daniel Bradby <daniel@dbradby.net>
2003-07-08 13:08:45 +0000

    added support for SVN:// URLs (untested but the URL validates now)

---
+++ core/src/org/tigris/subversion/subclipse/core/util/SvnURLHandler.java
@@ -0,0 +1,15 @@
+package org.tigris.subversion.subclipse.core.util;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+
+public class SvnURLHandler extends URLStreamHandler {
+
+	protected URLConnection openConnection(URL arg0) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
