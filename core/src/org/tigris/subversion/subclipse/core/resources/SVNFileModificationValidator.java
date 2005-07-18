d673be2a6c7554fabc0fdb545d1bdf7bd766f430
Mark Phippard <markphip@gmail.com>
2005-07-18 15:25:40 +0000

    Fixed problem in validateEdit().  I think this is an Eclipse problem.  In theory, we should only be called, and passed, an array of resources that are read-only.  Under certain conditions, we get called with other resources.  An easy example, is take the PDE option to migrate a plugin from 2.x to 3.x.  When you do this, we are passed an array of files to lock, even though the files are not read only.
    To workaround this problem I just added a secondary cleansing of the array to
    remove any files that are not read only.

--- core/src/org/tigris/subversion/subclipse/core/resources/SVNFileModificationValidator.java
+++ core/src/org/tigris/subversion/subclipse/core/resources/SVNFileModificationValidator.java
@@ -1,5 +1,8 @@
 package org.tigris.subversion.subclipse.core.resources;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFileModificationValidator;
 import org.eclipse.core.runtime.IStatus;
@@ -19,30 +22,52 @@ public class SVNFileModificationValidator implements IFileModificationValidator 
 	    SVNTeamProvider svnTeamProvider = null;
 	    RepositoryProvider provider = RepositoryProvider.getProvider(files[0].getProject());
 	    if ((provider != null) && (provider instanceof SVNTeamProvider)) {
-	       if (context != null) {
-		       ISVNFileModificationValidatorPrompt svnFileModificationValidatorPrompt = 
-		           SVNProviderPlugin.getPlugin().getSvnFileModificationValidatorPrompt();
-		       if (svnFileModificationValidatorPrompt != null) {
-		           if (!svnFileModificationValidatorPrompt.prompt(files, context))
-		               return SVNStatus.CANCEL_STATUS;
-		           comment = svnFileModificationValidatorPrompt.getComment();
-		           stealLock = svnFileModificationValidatorPrompt.isStealLock();
-		       }
-	       }
-           svnTeamProvider = (SVNTeamProvider)provider;
-           LockResourcesCommand command = new LockResourcesCommand(svnTeamProvider.getSVNWorkspaceRoot(), files, stealLock, comment);
-           try { 
-               command.run(new NullProgressMonitor());
-            } catch (SVNException e) {
-                e.printStackTrace();
-                return SVNStatus.CANCEL_STATUS;
+            IFile[] readOnly = checkReadOnly(files);
+            if (readOnly.length > 0) {
+                if (context != null) {
+                    ISVNFileModificationValidatorPrompt svnFileModificationValidatorPrompt = 
+                        SVNProviderPlugin.getPlugin().getSvnFileModificationValidatorPrompt();
+                    if (svnFileModificationValidatorPrompt != null) {
+                        if (!svnFileModificationValidatorPrompt.prompt(readOnly, context))
+                            return SVNStatus.CANCEL_STATUS;
+                        comment = svnFileModificationValidatorPrompt.getComment();
+                        stealLock = svnFileModificationValidatorPrompt.isStealLock();
+                    }
+                }
+                svnTeamProvider = (SVNTeamProvider) provider;
+                LockResourcesCommand command = new LockResourcesCommand(svnTeamProvider.getSVNWorkspaceRoot(), readOnly, stealLock, comment);
+                try {
+                    command.run(new NullProgressMonitor());
+                } catch (SVNException e) {
+                    e.printStackTrace();
+                    return SVNStatus.CANCEL_STATUS;
+                }
             }
-       }
+        }
 	    return SVNStatus.OK_STATUS;
     }
 
     public IStatus validateSave(IFile file) {
         return SVNStatus.OK_STATUS;
+    }
+    
+    
+    /**
+     * This method does a second check on the files in the array
+     * to verify their readOnly status.  There seems to be an Eclipse
+     * bug under certain situations where we are passed files that do not have
+     * this flag set.  This method will remove them from the array.
+     */
+    private IFile[] checkReadOnly(IFile[] files) {
+        List fileList = new ArrayList(files.length);
+        for (int i = 0; i < files.length; i++) {
+    	    try {
+                if (SVNWorkspaceRoot.getSVNResourceFor(files[i]).getStatus().isReadOnly())
+                    fileList.add(files[i]);
+            } catch (SVNException e) {
+            }
+        }
+        return (IFile[]) fileList.toArray(new IFile[fileList.size()]);
     }
 
 }
