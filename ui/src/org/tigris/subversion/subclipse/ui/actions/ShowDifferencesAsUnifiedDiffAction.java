11afc27fbbcd6c6d8ce8a61e5c44d844d2e1829c
Mark Phippard <markphip@gmail.com>
2005-11-22 21:47:57 +0000

    You can now select two items in the Repository Browser and execute a Show Differences as Unified Diff action.  This brings up a dialog so that you can control which item is the From and which is the To as well as possibly override the revisions to compare.
    So this lets you produce a diff between two tags, or trunk and a branch etc..

---
+++ ui/src/org/tigris/subversion/subclipse/ui/actions/ShowDifferencesAsUnifiedDiffAction.java
@@ -0,0 +1,28 @@
+package org.tigris.subversion.subclipse.ui.actions;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.team.core.TeamException;
+import org.tigris.subversion.subclipse.core.ISVNRemoteFile;
+import org.tigris.subversion.subclipse.core.ISVNRemoteFolder;
+import org.tigris.subversion.subclipse.core.ISVNRemoteResource;
+import org.tigris.subversion.subclipse.ui.dialogs.ShowDifferencesAsUnifiedDiffDialog;
+
+public class ShowDifferencesAsUnifiedDiffAction extends SVNAction {
+
+	protected void execute(IAction action) throws InvocationTargetException, InterruptedException {
+		ISVNRemoteResource[] selectedResources = getSelectedRemoteResources();
+		ShowDifferencesAsUnifiedDiffDialog dialog = new ShowDifferencesAsUnifiedDiffDialog(getShell(), selectedResources);
+		dialog.open();
+	}
+
+	protected boolean isEnabled() throws TeamException {
+		ISVNRemoteResource[] selectedResources = getSelectedRemoteResources();
+		if (selectedResources.length != 2) return false;
+		if (selectedResources[0] instanceof ISVNRemoteFolder && selectedResources[1] instanceof ISVNRemoteFolder) return true;
+		if (selectedResources[0] instanceof ISVNRemoteFile && selectedResources[1] instanceof ISVNRemoteFile) return true;
+		return false;
+	}
+
+}
