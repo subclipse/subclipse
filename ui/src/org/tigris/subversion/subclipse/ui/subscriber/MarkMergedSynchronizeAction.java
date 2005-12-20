3dc99139f7216b0a2ba4138b7e6be802d0f8d3f3
Mark Phippard <markphip@gmail.com>
2005-12-20 17:51:32 +0000

    Do not show Mark as Merged option when there is an incoming change and the file has been deleted locally.  The valid options are Revert or Update. Issue #: 398

--- ui/src/org/tigris/subversion/subclipse/ui/subscriber/MarkMergedSynchronizeAction.java
+++ ui/src/org/tigris/subversion/subclipse/ui/subscriber/MarkMergedSynchronizeAction.java
@@ -1,9 +1,13 @@
 package org.tigris.subversion.subclipse.ui.subscriber;
 
+import java.util.Iterator;
+
 import org.eclipse.compare.structuremergeviewer.IDiffElement;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.team.core.synchronize.FastSyncInfoFilter;
 import org.eclipse.team.core.synchronize.SyncInfo;
-import org.eclipse.team.core.synchronize.FastSyncInfoFilter.SyncInfoDirectionFilter;
+import org.eclipse.team.ui.synchronize.ISynchronizeModelElement;
 import org.eclipse.team.ui.synchronize.ISynchronizePageConfiguration;
 import org.eclipse.team.ui.synchronize.SynchronizeModelAction;
 import org.eclipse.team.ui.synchronize.SynchronizeModelOperation;
@@ -18,7 +22,20 @@ public class MarkMergedSynchronizeAction extends SynchronizeModelAction {
 	 * @see org.eclipse.team.ui.synchronize.SynchronizeModelAction#getSyncInfoFilter()
 	 */
 	protected FastSyncInfoFilter getSyncInfoFilter() {
-		return new SyncInfoDirectionFilter(new int[] {SyncInfo.CONFLICTING});
+		return new FastSyncInfoFilter() {
+			public boolean select(SyncInfo info) {
+				SyncInfoDirectionFilter filter = new SyncInfoDirectionFilter(new int[] {SyncInfo.CONFLICTING});
+				if (!filter.select(info)) return false;
+			    IStructuredSelection selection = getStructuredSelection();
+			    Iterator iter = selection.iterator();
+			    while (iter.hasNext()) {
+			    	ISynchronizeModelElement element = (ISynchronizeModelElement)iter.next();
+			    	IResource resource = element.getResource();
+			    	if (!resource.exists()) return false;
+			    }
+                return true;
+			}
+		};
 	}
     protected SynchronizeModelOperation getSubscriberOperation(ISynchronizePageConfiguration configuration, IDiffElement[] elements) {
         return new MarkMergedSynchronizeOperation(configuration, elements);
