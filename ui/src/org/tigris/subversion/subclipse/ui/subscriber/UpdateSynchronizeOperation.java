ab98ad501abf1567d59a47f8be0576bf370a7188
Mark Phippard <markphip@gmail.com>
2006-01-04 15:53:19 +0000

    Fixed problem in Update operation from Synch view.  It was always updating to HEAD.  Now it updates to highest selected revision.

--- ui/src/org/tigris/subversion/subclipse/ui/subscriber/UpdateSynchronizeOperation.java
+++ ui/src/org/tigris/subversion/subclipse/ui/subscriber/UpdateSynchronizeOperation.java
@@ -16,9 +16,13 @@ import org.eclipse.compare.structuremergeviewer.IDiffElement;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.team.core.synchronize.SyncInfo;
 import org.eclipse.team.core.synchronize.SyncInfoSet;
+import org.eclipse.team.core.variants.IResourceVariant;
 import org.eclipse.team.ui.synchronize.ISynchronizePageConfiguration;
+import org.tigris.subversion.subclipse.core.ISVNRemoteResource;
 import org.tigris.subversion.subclipse.core.SVNTeamProvider;
+import org.tigris.subversion.subclipse.core.sync.SVNStatusSyncInfo;
 import org.tigris.subversion.subclipse.ui.operations.UpdateOperation;
 import org.tigris.subversion.svnclientadapter.SVNRevision;
 
@@ -35,7 +39,25 @@ public class UpdateSynchronizeOperation extends SVNSynchronizeOperation {
 	}
 
 	protected void run(SVNTeamProvider provider, SyncInfoSet set, IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-		new UpdateOperation(getPart(), resources, SVNRevision.HEAD, true).run();
+		SVNRevision revision = null;
+		SyncInfo[] syncInfos = set.getSyncInfos();
+		for (int i = 0; i < syncInfos.length; i++) {
+			SVNStatusSyncInfo syncInfo = (SVNStatusSyncInfo)syncInfos[i];
+			IResourceVariant remote = syncInfo.getRemote();
+			if (remote != null && remote instanceof ISVNRemoteResource) {
+				SVNRevision rev = ((ISVNRemoteResource)remote).getLastChangedRevision();
+				if (rev instanceof SVNRevision.Number) {
+					long nbr = ((SVNRevision.Number)rev).getNumber();
+					if (revision == null) revision = rev;
+					else {
+						long revisionNumber = ((SVNRevision.Number)revision).getNumber();
+						if (nbr > revisionNumber) revision = rev;
+					}
+				}
+			}
+		}
+		if (revision == null) revision = SVNRevision.HEAD;
+		new UpdateOperation(getPart(), resources, revision, true).run();
 	}
 
 }
