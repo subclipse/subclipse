02deeb0a540ce77498ff3212f211867ea5158b1f
Mark Phippard <markphip@gmail.com>
2006-07-12 15:21:11 +0000

    Change project and source headers to the EPL

--- core/src/org/tigris/subversion/subclipse/core/resources/LocalResourceAdapterFactory.java
+++ core/src/org/tigris/subversion/subclipse/core/resources/LocalResourceAdapterFactory.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * Copyright (c) 2004, 2006 Subclipse project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
- * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Contributors:
- *     Cédric Chabanois (cchabanois@ifrance.com) 
- *******************************************************************************/
+ *     Subclipse project committers - initial API and implementation
+ ******************************************************************************/
 package org.tigris.subversion.subclipse.core.resources;
 
 import org.eclipse.core.resources.IContainer;
