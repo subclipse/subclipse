982b5515fd686c245d3c5920c60e360ec5f71cf4
Mark Phippard <markphip@gmail.com>
2006-10-14 13:33:39 +0000

    Unescape URL using java.net.URLDecoder instead of custom code.
    Suggested by: Mathus Baptiste

--- core/src/org/tigris/subversion/subclipse/core/util/Util.java
+++ core/src/org/tigris/subversion/subclipse/core/util/Util.java
@@ -11,6 +11,9 @@
 package org.tigris.subversion.subclipse.core.util;
 
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.tigris.subversion.subclipse.core.ISVNLocalFolder;
@@ -103,60 +106,15 @@ public class Util {
 
 	/**
 	 * unescape UTF8/URL encoded strings
-	 * from: http://www.w3.org/International/unescape.java
 	 * 
 	 * @param s
 	 * @return
 	 */
 	public static String unescape(String s) {
-		StringBuffer sbuf = new StringBuffer () ;
-		int l  = s.length() ;
-		int ch = -1 ;
-		int b, sumb = 0;
-		for (int i = 0, more = -1 ; i < l ; i++) {
-			/* Get next byte b from URL segment s */
-			switch (ch = s.charAt(i)) {
-			case '%':
-				ch = s.charAt (++i) ;
-				int hb = (Character.isDigit ((char) ch) 
-						? ch - '0'
-								: 10+Character.toLowerCase((char) ch) - 'a') & 0xF ;
-				ch = s.charAt (++i) ;
-				int lb = (Character.isDigit ((char) ch)
-						? ch - '0'
-								: 10+Character.toLowerCase ((char) ch)-'a') & 0xF ;
-				b = (hb << 4) | lb ;
-				break ;
-			case '+':
-				b = ' ' ;
-				break ;
-			default:
-				b = ch ;
-			}
-			/* Decode byte b as UTF-8, sumb collects incomplete chars */
-			if ((b & 0xc0) == 0x80) {			// 10xxxxxx (continuation byte)
-				sumb = (sumb << 6) | (b & 0x3f) ;	// Add 6 bits to sumb
-				if (--more == 0) sbuf.append((char) sumb) ; // Add char to sbuf
-			} else if ((b & 0x80) == 0x00) {		// 0xxxxxxx (yields 7 bits)
-				sbuf.append((char) b) ;			// Store in sbuf
-			} else if ((b & 0xe0) == 0xc0) {		// 110xxxxx (yields 5 bits)
-				sumb = b & 0x1f;
-				more = 1;				// Expect 1 more byte
-			} else if ((b & 0xf0) == 0xe0) {		// 1110xxxx (yields 4 bits)
-				sumb = b & 0x0f;
-				more = 2;				// Expect 2 more bytes
-			} else if ((b & 0xf8) == 0xf0) {		// 11110xxx (yields 3 bits)
-				sumb = b & 0x07;
-				more = 3;				// Expect 3 more bytes
-			} else if ((b & 0xfc) == 0xf8) {		// 111110xx (yields 2 bits)
-				sumb = b & 0x03;
-				more = 4;				// Expect 4 more bytes
-			} else /*if ((b & 0xfe) == 0xfc)*/ {	// 1111110x (yields 1 bit)
-				sumb = b & 0x01;
-				more = 5;				// Expect 5 more bytes
-			}
-			/* We don't test if the UTF-8 encoding is well-formed */
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return s;
 		}
-		return sbuf.toString() ;
 	}	
 }
