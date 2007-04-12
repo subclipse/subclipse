6b6d56deb20f59915164c011db7752795aa18271
Mark Phippard <markphip@gmail.com>
2007-04-12 19:42:56 +0000

    Modify plugin to use new client adapter extension point

--- core/src/org/tigris/subversion/subclipse/core/SVNClientManager.java
+++ core/src/org/tigris/subversion/subclipse/core/SVNClientManager.java
@@ -14,15 +14,9 @@ import java.io.File;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.team.core.TeamException;
+import org.tigris.subversion.clientadapter.Activator;
 import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
-import org.tigris.subversion.svnclientadapter.SVNClientAdapterFactory;
 import org.tigris.subversion.svnclientadapter.SVNClientException;
-import org.tigris.subversion.svnclientadapter.commandline.CmdLineClientAdapterFactory;
-import org.tigris.subversion.svnclientadapter.javahl.JhlClientAdapterFactory;
-import org.tigris.subversion.svnclientadapter.svnkit.SvnKitClientAdapterFactory;
 
 /**
  * Handles the creation of SVNClients
@@ -33,8 +27,6 @@ public class SVNClientManager {
     private String svnClientInterface;  
     private File configDir = null;
     private boolean fetchChangePathOnDemand = true;
-    private boolean javahl = false;
-    private boolean svnkit = false;
     
     public void startup(IProgressMonitor monitor) throws CoreException {
     }
@@ -44,41 +36,14 @@ public class SVNClientManager {
 	}
 
     /**
-     * set the client interface to use, either
-     * SVNClientAdapterFactory.JAVAHL_CLIENT or
-     * SVNClientAdapterFactory.SVNCOMMANDLINE_CLIENT
+     * set the client interface to use
      * 
      * @param svnClientInterface
      */
     public void setSvnClientInterface(String svnClientInterface) {
-        if (svnClientInterface == null) {
-          // if no specific interface is specified, load JavaHL
-          // if JavaHL fails to load, then JavaSVN will load
-            loadJavaHLAdapter();
-            try {
-                this.svnClientInterface = SVNClientAdapterFactory.getPreferredSVNClientType();
-            } catch (SVNClientException e) {
-                SVNProviderPlugin.log(new TeamException(new Status(IStatus.ERROR, SVNProviderPlugin.ID, IStatus.OK, e
-                        .getMessage(), e)));
-                return;
-            }
-        } else {
-	        if (CmdLineClientAdapterFactory.COMMANDLINE_CLIENT.equals(svnClientInterface))
-                svnClientInterface = SvnKitClientAdapterFactory.SVNKIT_CLIENT;
-	        if (JhlClientAdapterFactory.JAVAHL_CLIENT.equals(svnClientInterface))
-	            loadJavaHLAdapter();
-	        if (SvnKitClientAdapterFactory.SVNKIT_CLIENT.equals(svnClientInterface))
-	            loadSVNKitAdapter();
-	        if ("javasvn".equals(svnClientInterface))
-	            loadSVNKitAdapter();
-	        if (SVNClientAdapterFactory.isSVNClientAvailable(svnClientInterface)) {
-	            this.svnClientInterface = svnClientInterface;
-	        } else {
-	            if (this.svnClientInterface == null && SVNClientAdapterFactory.isSVNClientAvailable(SvnKitClientAdapterFactory.SVNKIT_CLIENT))
-	                this.svnClientInterface = SvnKitClientAdapterFactory.SVNKIT_CLIENT;
-	        }
-        }
-        
+
+    	this.svnClientInterface = svnClientInterface;
+    	
         // Initialize the admin directory name -- fixes a crash scenario with JavaHL
         // Since the method being called internally caches the result it
         // avoids a race condition later on, when someone runs an action like
@@ -92,9 +57,6 @@ public class SVNClientManager {
      * @return
      */
     public String getSvnClientInterface() {
-        if (svnClientInterface == null)
-            // force the adapters to load
-            setSvnClientInterface(null);
         return svnClientInterface;
     }    
     
@@ -110,17 +72,21 @@ public class SVNClientManager {
      * @throws SVNClientException
      */
     public ISVNClientAdapter createSVNClient() throws SVNException {
-        try {
-        	ISVNClientAdapter svnClient = SVNClientAdapterFactory.createSVNClient(getSvnClientInterface());
-        	if (configDir != null) {
-        		svnClient.setConfigDirectory(configDir);
-        	} 
-        	if (SVNProviderPlugin.getPlugin().getSvnPromptUserPassword() != null)
-        	    svnClient.addPasswordCallback(SVNProviderPlugin.getPlugin().getSvnPromptUserPassword());
-        	return svnClient;
-        } catch (SVNClientException e) {
-        	throw SVNException.wrapException(e);
-        }
+    	ISVNClientAdapter svnClient = Activator.getDefault().getClientAdapter(svnClientInterface);
+    	if (svnClient == null)
+    		svnClient = Activator.getDefault().getAnyClientAdapter();
+    	if (svnClient == null)
+    		throw new SVNException("No client adapters available.");
+    	if (configDir != null) {
+    		try {
+				svnClient.setConfigDirectory(configDir);
+			} catch (SVNClientException e) {
+	        	throw SVNException.wrapException(e);
+			}
+    	} 
+    	if (SVNProviderPlugin.getPlugin().getSvnPromptUserPassword() != null)
+    	    svnClient.addPasswordCallback(SVNProviderPlugin.getPlugin().getSvnPromptUserPassword());
+    	return svnClient;
     }    
 
 	/**
@@ -132,34 +98,10 @@ public class SVNClientManager {
 	/**
 	 * @param fetchChangePathOnDemand The fetchChangePathOnDemand to set.
 	 */
+	
 	public void setFetchChangePathOnDemand(
 			boolean fetchChangePathOnDemand) {
 		this.fetchChangePathOnDemand = fetchChangePathOnDemand;
 	}
 	
-	public void loadAdapters() {
-	    loadJavaHLAdapter();
-	    loadSVNKitAdapter();
-	}
-	
-	private void loadJavaHLAdapter() {
-        if (!javahl) {
-            javahl = true;
-		    try {
-	            JhlClientAdapterFactory.setup();
-	        } catch (SVNClientException e) {
-	            loadSVNKitAdapter(); // try to make sure there is at least one adapter available
-	        }
-        }
-	}
-	
-	private void loadSVNKitAdapter() {
-        if (!svnkit) {
-            svnkit = true;
-		    try {
-	            SvnKitClientAdapterFactory.setup();
-	        } catch (SVNClientException e) {
-	        }
-        }
-	}
 }
