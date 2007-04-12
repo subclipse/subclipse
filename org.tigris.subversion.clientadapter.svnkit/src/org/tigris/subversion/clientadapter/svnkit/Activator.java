bbdf37d1b873070e39e354639b256523e2ee02af
Mark Phippard <markphip@gmail.com>
2007-04-12 19:40:42 +0000

    SVNKit implementation of extension point
    NOTE: This one cannot really be used until SVNKit has a
    version that supports 1.5.

--- org.tigris.subversion.clientadapter.svnkit/src/org/tigris/subversion/clientadapter/svnkit/Activator.java
+++ org.tigris.subversion.clientadapter.svnkit/src/org/tigris/subversion/clientadapter/svnkit/Activator.java
@@ -2,17 +2,25 @@ package org.tigris.subversion.clientadapter.svnkit;
 
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
+import org.tigris.subversion.clientadapter.ISVNClientWrapper;
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
+import org.tigris.subversion.svnclientadapter.svnkit.SvnKitClientAdapter;
+import org.tigris.subversion.svnclientadapter.svnkit.SvnKitClientAdapterFactory;
+import org.tmatesoft.svn.core.javahl.SVNClientImpl;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends Plugin {
+public class Activator extends Plugin implements ISVNClientWrapper {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.tigris.subversion.clientadapter.svnkit";
 
 	// The shared instance
 	private static Activator plugin;
+
+	private String displayName;
+	private String version;
 	
 	/**
 	 * The constructor
@@ -46,5 +54,50 @@ public class Activator extends Plugin {
 	public static Activator getDefault() {
 		return plugin;
 	}
+
+	public ISVNClientAdapter getAdapter() {
+		if (this.isAvailable())
+			return new SvnKitClientAdapter();
+		else
+			return null;
+	}
+
+	public String getAdapterID() {
+		return SvnKitClientAdapterFactory.SVNKIT_CLIENT;
+	}
+
+	public String getVersionString() {
+		return getVersionSynchronized();
+	}
+
+	private synchronized String getVersionSynchronized() {
+		if (version == null) {
+			if (this.isAvailable()) {
+				SVNClientImpl adapter = SVNClientImpl.newInstance();
+				version = adapter.getVersion().toString();
+			} else
+				version = "Not Available";
+			}
+		return version;
+	}
+
+	public boolean isAvailable() {
+		return SvnKitClientAdapterFactory.isAvailable();
+	}
+
+	public void setDisplayName(String string) {
+		displayName = string;
+	}
+
+	public String getDisplayName() {
+		return displayName + " " + this.getVersionString();
+	}
+
+	public String getLoadErrors() {
+		if (this.isAvailable())
+			return "";
+		return "Class org.tmatesoft.svn.core.javahl.SVNClientImpl not found.\nInstall the SVNKit plug-in from http://www.svnkit.com/";
+	}
+
 
 }
