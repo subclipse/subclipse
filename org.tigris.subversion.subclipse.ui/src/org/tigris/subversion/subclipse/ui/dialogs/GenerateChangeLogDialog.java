37309b6efd807310f42bcca70769bc260a930086
Steve Elsemore <azulfool@gmail.com>
2008-01-04 21:16:08 +0000

    Add keyboard accelerators and fix text selection for Generate Change Log dialog. Issue #: 423

--- org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/dialogs/GenerateChangeLogDialog.java
+++ org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/dialogs/GenerateChangeLogDialog.java
@@ -9,6 +9,9 @@ import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.TrayDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusAdapter;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -187,6 +190,16 @@ public class GenerateChangeLogDialog extends TrayDialog {
 			clipboardButton.setSelection(true);
 			break;
 		}
+		
+		FocusListener focusListener = new FocusAdapter() {
+			public void focusGained(FocusEvent e) {
+				((Text)e.getSource()).selectAll();
+			}
+			public void focusLost(FocusEvent e) {
+				((Text)e.getSource()).setText(((Text)e.getSource()).getText());
+			}					
+		};
+		fileText.addFocusListener(focusListener);
 		
 		return composite;
 	}
