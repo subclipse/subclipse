a459321c0d48e1d68303fe94d2b87e27c67178e1
Steve Elsemore <azulfool@gmail.com>
2008-05-07 16:45:19 +0000

    Make export dialog remember the last location. Issue #: 729

--- org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/actions/ExportAction.java
+++ org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/actions/ExportAction.java
@@ -12,20 +12,32 @@ package org.tigris.subversion.subclipse.ui.actions;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.tigris.subversion.subclipse.ui.ISVNUIConstants;
 import org.tigris.subversion.subclipse.ui.Policy;
+import org.tigris.subversion.subclipse.ui.SVNUIPlugin;
 import org.tigris.subversion.subclipse.ui.operations.ExportOperation;
 
 public class ExportAction extends WorkbenchWindowAction {
+	private IDialogSettings settings;
+	private final static String settingsKey = "ExportAction.lastLocation"; //$NON-NLS-1$
+
+	public ExportAction() {
+		super();
+		settings = SVNUIPlugin.getPlugin().getDialogSettings();
+	}
 
 	protected void execute(IAction action) throws InvocationTargetException, InterruptedException {
 		DirectoryDialog dialog = new DirectoryDialog(getShell(), SWT.SAVE);
-		dialog.setText(Policy.bind("ExportAction.exportTo"));
+		dialog.setText(Policy.bind("ExportAction.exportTo")); //$NON-NLS-1$
+		dialog.setFilterPath(getLastLocation());
 		String directory = dialog.open();
 		if (directory == null) return;
+		saveLocation(directory);
 		new ExportOperation(getTargetPart(), getSelectedResources(), directory).run();
 	}
 
@@ -34,6 +46,29 @@ public class ExportAction extends WorkbenchWindowAction {
 	 */
 	protected String getImageId() {
 		return ISVNUIConstants.IMG_MENU_EXPORT;
+	}
+	
+	private String getLastLocation() {
+		String lastLocation = null;
+		IResource[] resources = getSelectedResources();
+		
+		// If just one resource selected, first see if this resource has
+		// previously been exported.
+		if (resources.length == 1) {
+			lastLocation = settings.get(settingsKey + "." + resources[0].getFullPath()); //$NON-NLS-1$
+		}
+		if (lastLocation == null) {
+			lastLocation = settings.get(settingsKey);
+		}
+		return lastLocation;
+	}
+	
+	private void saveLocation(String directory) {
+		IResource[] resources = getSelectedResources();
+		if (resources.length == 1) {
+			settings.put(settingsKey + "." + resources[0].getFullPath(), directory); //$NON-NLS-1$
+		}
+		settings.put(settingsKey, directory);
 	}
 
 }
