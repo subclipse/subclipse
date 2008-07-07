b5468027905fda9a543c0edab705a784325278ad
Mark Phippard <markphip@gmail.com>
2008-07-07 16:22:23 +0000

    Add an extension point so that a plug-in can provide a custom SSH provider for SVNKit.
    Patch by: Atsuhiko Yamanaka
    
    Issue #: 750

--- org.tigris.subversion.clientadapter.svnkit/src/org/tigris/subversion/clientadapter/svnkit/Activator.java
+++ org.tigris.subversion.clientadapter.svnkit/src/org/tigris/subversion/clientadapter/svnkit/Activator.java
@@ -1,11 +1,19 @@
 package org.tigris.subversion.clientadapter.svnkit;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.osgi.util.NLS;
 import org.osgi.framework.BundleContext;
 import org.tigris.subversion.clientadapter.ISVNClientWrapper;
 import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
 import org.tigris.subversion.svnclientadapter.svnkit.SvnKitClientAdapter;
 import org.tigris.subversion.svnclientadapter.svnkit.SvnKitClientAdapterFactory;
+import org.tmatesoft.svn.core.internal.io.svn.ISVNConnectorFactory;
 import org.tmatesoft.svn.core.javahl.SVNClientImpl;
 
 /**
@@ -16,11 +24,17 @@ public class Activator extends Plugin implements ISVNClientWrapper {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.tigris.subversion.clientadapter.svnkit";
 
+	// Extension point
+	public static final String PT_SVNCONNECTORFACTORY = "svnconnectorfactory"; //$NON-NLS-1$
+
 	// The shared instance
 	private static Activator plugin;
 
 	private String displayName;
 	private String version;
+
+	private ISVNConnectorFactory factory=null;
+	private boolean runOnce = false;
 	
 	/**
 	 * The constructor
@@ -57,7 +71,7 @@ public class Activator extends Plugin implements ISVNClientWrapper {
 
 	public ISVNClientAdapter getAdapter() {
 		if (this.isAvailable())
-			return new SvnKitClientAdapter();
+			return new SvnKitClientAdapter(null, null, getPluggedInSVNConnectorFactor());
 		else
 			return null;
 	}
@@ -99,5 +113,39 @@ public class Activator extends Plugin implements ISVNClientWrapper {
 		return "Class org.tmatesoft.svn.core.javahl.SVNClientImpl not found.\nInstall the SVNKit plug-in from http://www.svnkit.com/";
 	}
 
+	private ISVNConnectorFactory getPluggedInSVNConnectorFactor() {
+		if (!runOnce) {
+			runOnce = true;
+			IExtension[] extensions = Platform.getExtensionRegistry().getExtensionPoint(PLUGIN_ID, PT_SVNCONNECTORFACTORY).getExtensions();
+			for(int i=0; i<extensions.length; i++) {
+				IExtension extension = extensions[i];
+				IConfigurationElement[] configs = extension.getConfigurationElements();
+				if (configs.length == 0) {
+					Activator.log(IStatus.ERROR, NLS.bind("SVNConnectorFactory {0} is missing required fields", new Object[] {extension.getUniqueIdentifier()}), null);//$NON-NLS-1$ 
+					continue;
+				}
+				try {
+					IConfigurationElement config = configs[0];
+					factory=(ISVNConnectorFactory)config.createExecutableExtension("run");//$NON-NLS-1$ 
+				} catch (CoreException ex) {
+					System.err.println(ex);
+					Activator.log(IStatus.ERROR, NLS.bind("Could not instantiate SVNConnectorFactory for  {0}", new Object[] {extension.getUniqueIdentifier()}), ex);//$NON-NLS-1$ 
+				}
+			}
+		}
+		return factory;
+	}
+
+    /**
+     * Log the given exception along with the provided message and severity indicator
+     */
+    public static void log(int severity, String message, Throwable e) {
+        log(new Status(severity, PLUGIN_ID, 0, message, e));
+    }
+
+    public static void log(IStatus status) {
+		// For now, we'll log the status. However we should do more
+		getDefault().getLog().log(status);
+	}
 
 }
