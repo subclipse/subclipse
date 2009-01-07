86535b6bacac96a162d8e9c4aa3f7061e22a0924
Steve Elsemore <azulfool@gmail.com>
2009-01-07 20:19:39 +0000

    Show dialog when Mark Resolved option is run so user can choose how to resolve. (Sync view implementation). Issue #: 826

--- org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/subscriber/ResolveSynchronizeOperation.java
+++ org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/subscriber/ResolveSynchronizeOperation.java
@@ -13,6 +13,7 @@ package org.tigris.subversion.subclipse.ui.subscriber;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.compare.structuremergeviewer.IDiffElement;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -30,8 +31,16 @@ import org.tigris.subversion.subclipse.core.ISVNLocalResource;
 import org.tigris.subversion.subclipse.core.SVNTeamProvider;
 import org.tigris.subversion.subclipse.core.resources.SVNWorkspaceRoot;
 import org.tigris.subversion.subclipse.ui.SVNUIPlugin;
+import org.tigris.subversion.subclipse.ui.wizards.dialogs.SvnWizard;
+import org.tigris.subversion.subclipse.ui.wizards.dialogs.SvnWizardDialog;
+import org.tigris.subversion.subclipse.ui.wizards.dialogs.SvnWizardMarkResolvedPage;
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
+import org.tigris.subversion.svnclientadapter.ISVNConflictResolver;
+import org.tigris.subversion.svnclientadapter.SVNClientException;
 
 public class ResolveSynchronizeOperation extends SVNSynchronizeOperation {
+	private boolean canceled;
+	private int selectedResolution;
     
 	public final static int PROGRESS_DIALOG = 1;
 	public final static int PROGRESS_BUSYCURSOR = 2;
@@ -45,20 +54,49 @@ public class ResolveSynchronizeOperation extends SVNSynchronizeOperation {
     }
 
     protected void run(SVNTeamProvider provider, SyncInfoSet set, IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-        final IResource[] resources = set.getResources();
+    	boolean folderSelected = false;  
+    	canceled = false;
+    	final IResource[] resources = set.getResources();
+		for (int i = 0; i < resources.length; i++) {
+			if (resources[i] instanceof IContainer) {
+				folderSelected = true;
+				break;
+			}
+		}
+		if (folderSelected) {
+			selectedResolution = ISVNConflictResolver.Choice.chooseMerged;
+		} else {
+			Display.getDefault().syncExec(new Runnable() {
+				public void run() {
+					SvnWizardMarkResolvedPage markResolvedPage = new SvnWizardMarkResolvedPage(resources);
+					SvnWizard wizard = new SvnWizard(markResolvedPage);
+			        SvnWizardDialog dialog = new SvnWizardDialog(getShell(), wizard);
+			        wizard.setParentDialog(dialog);
+			        if (dialog.open() == SvnWizardDialog.CANCEL) {
+			        	canceled = true;
+			        	return;
+			        }
+			        selectedResolution = markResolvedPage.getResolution();					
+				}				
+			});
+		}
+		if (canceled) return;
 		run(new WorkspaceModifyOperation() {
 			public void execute(IProgressMonitor monitor) throws InvocationTargetException {
 				
 				try {
 					for (int i = 0; i < resources.length; i++) {						
                         ISVNLocalResource svnResource = SVNWorkspaceRoot.getSVNResourceFor(resources[i]);
-                        svnResource.resolve();
+                        ISVNClientAdapter svnClient = svnResource.getRepository().getSVNClient();
+                        svnClient.resolve(resources[i].getLocation().toFile(), selectedResolution);
                         //for some reason, just refreshing the file won't cut it.
                         resources[i].getParent().refreshLocal(IResource.DEPTH_INFINITE, monitor);
 					}
 				} catch (TeamException e) {
 					throw new InvocationTargetException(e);
 				} catch (CoreException e) {
+					throw new InvocationTargetException(e);
+				} catch (SVNClientException e) {
 					throw new InvocationTargetException(e);
 				}
 			}
