2c04b6e11ca8f10a52841a133db3cbbb189bd85f
Steve Elsemore <azulfool@gmail.com>
2009-01-16 15:33:06 +0000

    Tree Conflicts View stub.  Need a new view icon.  Current icon is a copy of SVN properties view icon

---
+++ org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/conflicts/TreeConflictsView.java
@@ -0,0 +1,22 @@
+package org.tigris.subversion.subclipse.ui.conflicts;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class TreeConflictsView extends ViewPart {
+	public static final String VIEW_ID = "org.tigris.subversion.subclipse.ui.conflicts.TreeConflictsView"; //$NON-NLS-1$
+
+	public TreeConflictsView() {
+		super();
+	}
+
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setFocus() {
+//		treeViewer.getControl().setFocus();
+	}
+
+}
