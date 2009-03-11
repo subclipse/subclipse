bbaafa4c671df914acee2a32d33f16279c368cc0
Steve Elsemore <azulfool@gmail.com>
2009-03-11 22:54:47 +0000

    Invoke Resolve Tree Conflict dialog when a resource with a tree conflict status is selected for Mark Resolved option. Issue #: 826

--- org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/actions/ResolveActionWithChoices.java
+++ org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/actions/ResolveActionWithChoices.java
@@ -7,25 +7,35 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.widgets.Display;
 import org.tigris.subversion.subclipse.core.ISVNLocalResource;
 import org.tigris.subversion.subclipse.core.SVNException;
 import org.tigris.subversion.subclipse.core.resources.LocalResourceStatus;
+import org.tigris.subversion.subclipse.core.resources.SVNTreeConflict;
 import org.tigris.subversion.subclipse.core.resources.SVNWorkspaceRoot;
 import org.tigris.subversion.subclipse.ui.Policy;
 import org.tigris.subversion.subclipse.ui.SVNUIPlugin;
+import org.tigris.subversion.subclipse.ui.conflicts.ResolveTreeConflictWizard;
+import org.tigris.subversion.subclipse.ui.wizards.SizePersistedWizardDialog;
 import org.tigris.subversion.subclipse.ui.wizards.dialogs.SvnWizard;
 import org.tigris.subversion.subclipse.ui.wizards.dialogs.SvnWizardDialog;
 import org.tigris.subversion.subclipse.ui.wizards.dialogs.SvnWizardMarkResolvedPage;
+import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
 import org.tigris.subversion.svnclientadapter.ISVNConflictResolver;
+import org.tigris.subversion.svnclientadapter.ISVNStatus;
 
 public class ResolveActionWithChoices extends ResolveAction {
 	private int selectedResolution;
+	private SVNTreeConflict treeConflict;
 	
 	protected void execute(IAction action) throws InvocationTargetException, InterruptedException {
 		boolean folderSelected = false;
 		boolean propertyConflicts = false;
 		boolean textConflicts = false;
 		boolean treeConflicts = false;
+		boolean treeConflictDialogShown = false;
 		IResource[] resources = getSelectedResources();
 		for (int i = 0; i < resources.length; i++) {
 			if (resources[i] instanceof IContainer) {
@@ -48,24 +58,55 @@ public class ResolveActionWithChoices extends ResolveAction {
 			selectedResolution = ISVNConflictResolver.Choice.chooseMerged;
 			setResolution(selectedResolution);
 		} else {
-			if (propertyConflicts && !textConflicts) {
-				String message;
-				if (resources.length > 1) message = Policy.bind("ResolveAction.confirmMultiple"); //$NON-NLS-1$
-				else message = Policy.bind("ResolveAction.confirm", resources[0].getName()); //$NON-NLS-1$
-				if (!MessageDialog.openConfirm(getShell(), Policy.bind("ResolveOperation.taskName"), message)) return; //$NON-NLS-1$
-				setResolution(ISVNConflictResolver.Choice.chooseMerged);				
-			} else {
-				SvnWizardMarkResolvedPage markResolvedPage = new SvnWizardMarkResolvedPage(resources);
-				markResolvedPage.setPropertyConflicts(propertyConflicts);
-				markResolvedPage.setTreeConflicts(treeConflicts);
-				SvnWizard wizard = new SvnWizard(markResolvedPage);
-		        SvnWizardDialog dialog = new SvnWizardDialog(getShell(), wizard);
-		        wizard.setParentDialog(dialog);
-		        if (dialog.open() == SvnWizardDialog.CANCEL) return;
-		        setResolution(markResolvedPage.getResolution());
+			if (resources.length == 1 && treeConflicts && !propertyConflicts && !textConflicts) {
+				treeConflict = getTreeConflict(resources[0]);
+				if (treeConflict != null) {
+					ResolveTreeConflictWizard wizard = new ResolveTreeConflictWizard(treeConflict, getTargetPart());
+					WizardDialog dialog = new SizePersistedWizardDialog(Display.getDefault().getActiveShell(), wizard, "ResolveTreeConflict"); //$NON-NLS-1$
+					if (dialog.open() != WizardDialog.OK) return;
+					treeConflictDialogShown = true;
+				}
+			}
+			if (!treeConflictDialogShown) {
+				if (propertyConflicts && !textConflicts) {
+					String message;
+					if (resources.length > 1) message = Policy.bind("ResolveAction.confirmMultiple"); //$NON-NLS-1$
+					else message = Policy.bind("ResolveAction.confirm", resources[0].getName()); //$NON-NLS-1$
+					if (!MessageDialog.openConfirm(getShell(), Policy.bind("ResolveOperation.taskName"), message)) return; //$NON-NLS-1$
+					setResolution(ISVNConflictResolver.Choice.chooseMerged);				
+				} else {
+					SvnWizardMarkResolvedPage markResolvedPage = new SvnWizardMarkResolvedPage(resources);
+					markResolvedPage.setPropertyConflicts(propertyConflicts);
+					markResolvedPage.setTreeConflicts(treeConflicts);
+					SvnWizard wizard = new SvnWizard(markResolvedPage);
+			        SvnWizardDialog dialog = new SvnWizardDialog(getShell(), wizard);
+			        wizard.setParentDialog(dialog);
+			        if (dialog.open() == SvnWizardDialog.CANCEL) return;
+			        setResolution(markResolvedPage.getResolution());
+				}
 			}
 		}
-		super.execute(action);
+		if (!treeConflictDialogShown) super.execute(action);
 	}
+	
+	private SVNTreeConflict getTreeConflict(final IResource resource) {
+		BusyIndicator.showWhile(Display.getDefault(), new Runnable() {
+			public void run() {
+				try {
+					ISVNClientAdapter client = SVNWorkspaceRoot.getSVNResourceFor(resource).getRepository().getSVNClient();
+					ISVNStatus[] statuses = client.getStatus(resource.getLocation().toFile(), true, true, true);
+					for (int i = 0; i < statuses.length; i++) {
+						if (statuses[i].hasTreeConflict()) {
+							treeConflict = new SVNTreeConflict(statuses[i]);
+							break;
+						}
+					}
+				} catch (Exception e) {
+					SVNUIPlugin.log(IStatus.ERROR, e.getMessage(), e);
+				}
+			}			
+		});
+		return treeConflict;
+	}	
 
 }
