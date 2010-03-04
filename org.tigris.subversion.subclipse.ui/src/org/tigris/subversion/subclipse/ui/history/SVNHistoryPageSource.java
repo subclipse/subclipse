a3da3c233d009850efa6d9496309e16b8fbf8f73
Steve Elsemore <azulfool@gmail.com>
2010-03-04 19:33:17 +0000

    Fix null argument error in history view when link with selection/editor is turned on and the history view is currently opened on a remote resource and an added or unmanaged file is opened. Issue #: 1104

--- org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/history/SVNHistoryPageSource.java
+++ org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/history/SVNHistoryPageSource.java
@@ -11,9 +11,13 @@
 package org.tigris.subversion.subclipse.ui.history;
 
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.team.ui.history.HistoryPageSource;
 import org.eclipse.ui.part.Page;
+import org.tigris.subversion.subclipse.core.ISVNLocalResource;
 import org.tigris.subversion.subclipse.core.ISVNRemoteResource;
+import org.tigris.subversion.subclipse.core.resources.SVNWorkspaceRoot;
+import org.tigris.subversion.subclipse.ui.SVNUIPlugin;
 
 /**
  * <code>IHistoryPageSource</code> implementation for Subclipse history
@@ -23,6 +27,19 @@ import org.tigris.subversion.subclipse.core.ISVNRemoteResource;
 public class SVNHistoryPageSource extends HistoryPageSource {
 
   public boolean canShowHistoryFor(Object object) {
+	if (object instanceof IResource) {
+		IResource resource = (IResource)object;
+		ISVNLocalResource localResource = SVNWorkspaceRoot.getSVNResourceFor(resource);
+		if (localResource != null) {
+			try {
+				if (!localResource.isManaged() || localResource.isAdded()) {
+					return false;
+				}
+			} catch (Exception e) {
+				SVNUIPlugin.log(Status.ERROR, e.getMessage(), e);
+			}
+		}
+	}
     return (object instanceof IResource && ((IResource) object).getType() != IResource.ROOT)
         || (object instanceof ISVNRemoteResource);
   }
