4a885b9587753d2f6543f092436702f84f833a48
Steve Elsemore <azulfool@gmail.com>
2010-09-29 22:27:15 +0000

    Fix incorrect bundle name in messages class.

--- org.tigris.subversion.subclipse.tools.usage/src/org/tigris/subversion/subclipse/tools/usage/tracker/internal/TrackerMessages.java
+++ org.tigris.subversion.subclipse.tools.usage/src/org/tigris/subversion/subclipse/tools/usage/tracker/internal/TrackerMessages.java
@@ -3,7 +3,7 @@ package org.tigris.subversion.subclipse.tools.usage.tracker.internal;
 import org.eclipse.osgi.util.NLS;
 
 public class TrackerMessages extends NLS {
-	private static final String BUNDLE_NAME = "org.tigris.subversion.subclipse.tools.usage.tracker.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.tigris.subversion.subclipse.tools.usage.tracker.internal.messages"; //$NON-NLS-1$
 	public static String Tracker_Synchronous;
 	public static String Tracker_Asynchronous;
 	public static String Tracker_Error;
