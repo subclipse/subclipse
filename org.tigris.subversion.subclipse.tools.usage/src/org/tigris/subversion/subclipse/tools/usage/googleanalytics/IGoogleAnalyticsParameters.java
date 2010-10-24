ba4873692e94c9c13dc45fc1a06d2178c3f71227
Steve Elsemore <azulfool@gmail.com>
2010-10-24 03:48:41 +0000

    Add copyright headers

--- org.tigris.subversion.subclipse.tools.usage/src/org/tigris/subversion/subclipse/tools/usage/googleanalytics/IGoogleAnalyticsParameters.java
+++ org.tigris.subversion.subclipse.tools.usage/src/org/tigris/subversion/subclipse/tools/usage/googleanalytics/IGoogleAnalyticsParameters.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Subclipse project and others.
+ * Copyright (c) 2010 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Subclipse project committers
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.tigris.subversion.subclipse.tools.usage.googleanalytics;
 
 public interface IGoogleAnalyticsParameters {
