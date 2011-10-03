146a26f1f69211868a2bbab9cd3042aee1d1bd33
Steve Elsemore <azulfool@gmail.com>
2011-10-03 17:11:18 +0000

    Check for test mode (System property "eclipse.application" ends with "testapplication" or system property "eclipse.commands" contains "testapplication\n") and avoid modal dialogs if true.
    Applying proposed fix from Steffen Pingel.

--- org.tigris.subversion.subclipse.tools.usage/src/org/tigris/subversion/subclipse/tools/usage/reporting/UsageReportDispatcher.java
+++ org.tigris.subversion.subclipse.tools.usage/src/org/tigris/subversion/subclipse/tools/usage/reporting/UsageReportDispatcher.java
@@ -20,8 +20,29 @@ import org.tigris.subversion.subclipse.tools.usage.util.LoggingUtils;
 import org.tigris.subversion.subclipse.tools.usage.util.StatusUtils;
 
 public class UsageReportDispatcher implements IStartup {
+	public static final boolean TEST_MODE;
+
+	  static {
+	    String application = System.getProperty("eclipse.application", "");
+	    if (application.length() > 0) {
+	    	
+	      System.out.println("application: " + application);
+	    	
+	      TEST_MODE = application.endsWith("testapplication") || application.endsWith("uitest");
+	    } else {
+	       String commands = System.getProperty("eclipse.commands", "");
+	       
+	       System.out.println("commands: " + commands);
+	       
+	       TEST_MODE = commands.contains("testapplication\n");
+	    }
+	  }
 
 	public void earlyStartup() {
+		if (TEST_MODE) {
+			return;
+		}
+		
 		Display.getDefault().asyncExec(new Runnable() {
 
 			public void run() {
