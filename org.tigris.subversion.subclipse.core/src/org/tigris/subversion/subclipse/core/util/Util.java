38c616826c5ad920f67bd10253497d16adce235f
Steve Elsemore <azulfool@gmail.com>
2011-12-20 17:53:51 +0000

    If "Ignore changes to hidden resources" preference is true, then do not include resources that have been filtered out using Resource Filters when doing a commit, revert or synchronize. Issue #: 1321

--- org.tigris.subversion.subclipse.core/src/org/tigris/subversion/subclipse/core/util/Util.java
+++ org.tigris.subversion.subclipse.core/src/org/tigris/subversion/subclipse/core/util/Util.java
@@ -152,6 +152,12 @@ public class Util {
 	}
 	
 	public static boolean isHidden(IResource resource) {
+		
+		// If resource is excluded using resource filters, return true.
+		if (!resource.exists() && resource.getLocation() != null && resource.getLocation().toFile() != null && resource.getLocation().toFile().exists()) {
+			return true;
+		}
+		
 		// If we've previously checked for isHidden method and it is not supported, return false.
 		if (isHiddenUnsupported) {
 			return false;
@@ -170,6 +176,13 @@ public class Util {
 		IResource parent = resource;
 		while (parent != null) {			
 			try {
+				
+				if (parent.getName().equals("Expressions")) {
+					System.out.println("location: " + parent.getLocation());
+					System.out.println("accessible: " + parent.isAccessible());
+					System.out.println("exists: " + parent.exists());
+				}
+				
 				Object isHidden = isHiddenMethod.invoke(parent, new Object[] {});
 				if (isHidden instanceof Boolean) {
 					if (((Boolean)isHidden).booleanValue()) {
