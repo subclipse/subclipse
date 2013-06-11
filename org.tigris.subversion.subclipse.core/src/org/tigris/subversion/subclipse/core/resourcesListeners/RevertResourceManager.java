9a6a9a780a8ca8196956e57a77a782729b9976e5
Steve Elsemore <azulfool@gmail.com>
2013-06-11 19:53:29 +0000

    Fix potential deadlocks between RevertResourceManager jobs and other Subclipse jobs. Issue #: 1523

--- org.tigris.subversion.subclipse.core/src/org/tigris/subversion/subclipse/core/resourcesListeners/RevertResourceManager.java
+++ org.tigris.subversion.subclipse.core/src/org/tigris/subversion/subclipse/core/resourcesListeners/RevertResourceManager.java
@@ -11,20 +11,18 @@ import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
-import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.jobs.MultiRule;
 import org.tigris.subversion.subclipse.core.ISVNLocalFile;
 import org.tigris.subversion.subclipse.core.ISVNLocalFolder;
 import org.tigris.subversion.subclipse.core.ISVNLocalResource;
-import org.tigris.subversion.subclipse.core.Policy;
 import org.tigris.subversion.subclipse.core.SVNException;
 import org.tigris.subversion.subclipse.core.SVNProviderPlugin;
 import org.tigris.subversion.subclipse.core.client.OperationManager;
 import org.tigris.subversion.subclipse.core.resources.SVNMoveDeleteHook;
 import org.tigris.subversion.subclipse.core.resources.SVNWorkspaceRoot;
+import org.tigris.subversion.subclipse.core.util.JobUtility;
 import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
 import org.tigris.subversion.svnclientadapter.SVNClientException;
 /**
@@ -33,60 +31,6 @@ import org.tigris.subversion.svnclientadapter.SVNClientException;
  * @author Hugo Visser joegi at scene.nl
  */
 public class RevertResourceManager implements IResourceChangeListener {
-
-	private class RevertWorkspaceJob extends WorkspaceJob {
-		
-		private final ISVNLocalResource[] resources;
-
-		public RevertWorkspaceJob(ISVNLocalResource[] resources) {
-			super(Policy.bind("RevertResourceManager.jobName"));
-			this.resources = resources;
-		}
-
-		private void revertResources(ISVNLocalResource[] resources, IProgressMonitor monitor) throws SVNException {
-			monitor.beginTask(Policy.bind("RevertResourceManager.reverting"), resources.length);
-			for (int i=0; i < resources.length; i++) {
-				if (monitor.isCanceled()) {
-					break;
-				}
-				if (resources[i] instanceof ISVNLocalFile) {
-					ISVNLocalFile res = (ISVNLocalFile)resources[i];
-					File file = res.getFile().getAbsoluteFile();
-	    	        // TODO a better location?
-	    	        File tmp = new File(res.getFile().getAbsolutePath() + ".svntmp");
-	    	        if (tmp.exists()) {
-	    	        	tmp.delete();
-	    	        }
-	    	        if (file.renameTo(tmp)) {
-	    	            // res.revert is recursive and updates local history
-	    	            revert(res);
-	    	            if (!file.delete()) {
-	    	                //TODO handle this in a better way?
-	    	                throw SVNException.wrapException(new IllegalStateException("Could not remove "+file));                                                            	
-	    	            }
-	    	            if (!tmp.renameTo(file)) {
-	    	                //TODO handle this in a better way?
-	    	                throw SVNException.wrapException(new IllegalStateException("Could not rename "+tmp+" to "+file));                                
-	    	            }
-	    	        } else {
-	    	            //TODO handle this in a better way?
-	    	            throw SVNException.wrapException(new IllegalStateException("Could not rename "+file+" to "+tmp));
-	    	        }					
-				}
-				else {
-					revert(resources[i]);
-				}
-				monitor.worked(1);
-			}
-			monitor.done();
-		}
-
-		public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
-			revertResources(resources, monitor);
-			return monitor.isCanceled() ? Status.CANCEL_STATUS : Status.OK_STATUS;
-		}
-
-	}
 
 	public void resourceChanged(IResourceChangeEvent event) {		
         final List<IResourceDelta> addedFileResources = new ArrayList<IResourceDelta>();
@@ -123,9 +67,64 @@ public class RevertResourceManager implements IResourceChangeListener {
             if (!addedFileResources.isEmpty()) {
                 final IResourceDelta[] resources = (IResourceDelta[]) addedFileResources.toArray(new IResourceDelta[addedFileResources
                                                                                                      .size()]);                
-                ISVNLocalResource[] revertResources = processResources(resources);
+                final ISVNLocalResource[] revertResources = processResources(resources);
                 if (revertResources.length > 0) {
-                  	new RevertWorkspaceJob(revertResources).schedule(500);
+                	
+                	List<IProject> projects = new ArrayList<IProject>();
+                	for (ISVNLocalResource revertResource : revertResources) {
+                		IProject project = revertResource.getResource().getProject();
+						if (project != null && !projects.contains(project)) {
+							projects.add(project);
+						}
+                	}
+    				IProject[] projectArray = new IProject[projects.size()];
+    				projects.toArray(projectArray);
+
+    				JobUtility.scheduleJob("Revert Resources", new Runnable() {				
+    					public void run() {
+    						try {
+	    						for (int i=0; i < revertResources.length; i++) {
+	    							if (revertResources[i] instanceof ISVNLocalFile) {
+	    								ISVNLocalFile res = (ISVNLocalFile)revertResources[i];
+	    								
+	    								File file = res.getFile().getAbsoluteFile();
+	    				    	        // TODO a better location?
+	    				    	        File tmp = new File(res.getFile().getAbsolutePath() + ".svntmp");
+	    				    	        if (tmp.exists()) {
+	    				    	        	tmp.delete();
+	    				    	        }
+	    				    	        if (file.renameTo(tmp)) {
+	    				    	            // res.revert is recursive and updates local history
+	    				    	            revert(res);
+	    				    	            if (!file.delete()) {
+	    				    	                //TODO handle this in a better way?
+	    				    	            	if (file.exists()) {
+	    				    	            		throw SVNException.wrapException(new IllegalStateException("Could not remove "+file));  
+	    				    	            	}
+	    				    	            }
+	    				    	            if (!tmp.renameTo(file)) {
+	    				    	                //TODO handle this in a better way?
+	    				    	                throw SVNException.wrapException(new IllegalStateException("Could not rename "+tmp+" to "+file));                                
+	    				    	            }
+	    				    	            
+	    				    	            try {
+												res.getParent().getResource().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+											} catch (CoreException e) {}
+	    				    	            
+	    				    	        } else {
+	    				    	            //TODO handle this in a better way?
+	    				    	            throw SVNException.wrapException(new IllegalStateException("Could not rename "+file+" to "+tmp));
+	    				    	        }					
+	    							}
+	    							else {
+	    								revert(revertResources[i]);
+	    							}
+	    						}
+    						} catch (SVNException e) {
+    							SVNProviderPlugin.log(e.getStatus());
+    						}
+    					}
+    				}, MultiRule.combine(projectArray), false);
                 }
             }
         } catch (CoreException e) {
