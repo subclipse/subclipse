f90ead84b2d8549fa4f1a6752441af248aeb4c0f
Steve Elsemore <azulfool@gmail.com>
2013-08-22 16:59:45 +0000

    Don't show multiple prompts when files from multiple projects added to svn:ignore from sync view. Issue#: 1543

--- org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/subscriber/IgnoreSynchronizeOperation.java
+++ org.tigris.subversion.subclipse.ui/src/org/tigris/subversion/subclipse/ui/subscriber/IgnoreSynchronizeOperation.java
@@ -11,14 +11,20 @@
 package org.tigris.subversion.subclipse.ui.subscriber;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.eclipse.compare.structuremergeviewer.IDiffElement;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.team.core.RepositoryProvider;
 import org.eclipse.team.core.synchronize.SyncInfoSet;
 import org.eclipse.team.ui.synchronize.ISynchronizePageConfiguration;
 import org.tigris.subversion.subclipse.core.SVNTeamProvider;
+import org.tigris.subversion.subclipse.ui.Policy;
+import org.tigris.subversion.subclipse.ui.SVNUIPlugin;
 import org.tigris.subversion.subclipse.ui.dialogs.IgnoreResourcesDialog;
 import org.tigris.subversion.subclipse.ui.operations.IgnoreOperation;
 
@@ -34,6 +40,27 @@ public class IgnoreSynchronizeOperation extends SVNSynchronizeOperation {
 
 	protected boolean promptForConflictHandling(Shell shell, SyncInfoSet syncSet) {
 		return true;
+	}
+	
+	public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+		// First, ask the user if they want to include conflicts
+		SyncInfoSet syncSet = getSyncInfoSet();
+		if (!promptForConflictHandling(getShell(), syncSet)) return;
+		// Divide the sync info by project
+		final Map projectSyncInfos = getProjectSyncInfoSetMap(syncSet);
+		monitor.beginTask(null, projectSyncInfos.size() * 100);
+		for (Iterator iter = projectSyncInfos.keySet().iterator(); iter.hasNext(); ) {
+			final IProject project = (IProject) iter.next();
+
+			// Pass the scheduling rule to the synchronizer so that sync change events
+			// and cache commits to disk are batched
+			SVNTeamProvider provider = (SVNTeamProvider)RepositoryProvider.getProvider(project, SVNUIPlugin.PROVIDER_ID);
+			if (provider != null) {
+				run(provider, (SyncInfoSet)projectSyncInfos.get(project), Policy.subMonitorFor(monitor,100));
+				break;
+			}
+		}
+		monitor.done();
 	}
 
 	protected void run(SVNTeamProvider provider, SyncInfoSet set, IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
