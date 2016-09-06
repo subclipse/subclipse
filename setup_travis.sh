9e1b4ae2c49d52cb065b2752e6ee5c33367a21de
Mark Phippard <markphip@gmail.com>
2016-09-05 20:56:51 -0400

    Create Maven home folder before build starts
    It looks like this might be a requirement for the
    Tycho pomless build process

---
+++ setup_travis.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Create the .m2 folder if it does not exist
+[ -d $HOME/.m2 ] || mkdir $HOME/.m2
+
+# Copy the settings.xml file to .m2
+[ -d $HOME/.m2/settings.xml ] || rm $HOME/.m2/settings.xml
+cp ./travis_settings.xml $HOME/.m2/settings.xml
+
